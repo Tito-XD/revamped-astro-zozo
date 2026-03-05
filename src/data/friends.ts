5eb633643a71644876d6e19f5f07ae8e246ead8f
tito_xd <titow.xd@outlook.com>
2026-03-05 17:06:21 +0800

    friends pages update

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -7,10 +7,28 @@ export interface FriendLink {
 
 // Edit this list only, the /friends page will update automatically.
 export const friends: FriendLink[] = [
-	// {
-	// 	id: 'Alice Blog',
-	// 	avatar: 'https://example.com/avatar.jpg',
-	// 	bio: 'Design, writing, and frontend experiments.',
-	// 	link: 'https://example.com',
-	// },
+	{
+		id: 'Rachel',
+		avatar: 'https://rachelt.one/res/small_avatar.svg',
+		bio: 'Hi, would you like a cup of Rachel today?',
+		link: 'https://rachelt.one/zh/',
+	},
+	{
+		id: 'QPomelo',
+		avatar: 'https://qpomelo.dev/icons/avatar.jpeg',
+		bio: 'aka 柚子',
+		link: 'https://qpomelo.dev',
+	},
+	{
+		id: 'Heuluck',
+		avatar: 'https://avatars.githubusercontent.com/u/89375068?v=4',
+		bio: '',
+		link: 'https://hugo.heuluck.dev',
+	},
+	{
+		id: 'Wr',
+		avatar: 'https://avatars.githubusercontent.com/u/89375068?v=4',
+		bio: '',
+		link: 'https://wrye.dev/',
+	}
 ];
